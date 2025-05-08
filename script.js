f8d1e304f81b05c9c6346a82c7e7e444133eb899
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-08 18:37:39 +0600

    reg form

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    //step 1:
+
     const registrationBtn = document.getElementById('registration-btn');
     const loginBtn = document.getElementById('login-btn');
     const registrationTab = document.getElementById('registration-tab');
@@ -19,5 +22,29 @@ document.addEventListener('DOMContentLoaded', function() {
         loginTab.classList.add('active');
         registrationTab.classList.remove('active');
     });
+
     
+    //Step 2
+
+
+    const profileBtn = document.getElementById('profile-btn');
+    const dokumenteBtn= document.getElementById('dokumente-btn');
+    const profileTab= document.getElementById('profile-tab');
+    const dokumenteTab=document.getElementById('dokumente-tab');
+
+    dokumenteBtn.addEventListener('click',function(){
+        //switch to dokumente tab
+        dokumenteBtn.classList.add('active');
+        profileBtn.classList.remove('active');
+        dokumenteTab.classList.add('active');
+        profileTab.classList.remove('active');
+    });
+
+    profileBtn.addEventListener('click',function(){
+        //switeh to profile tab
+        profileBtn.classList.add('active');
+        dokumenteBtn.classList.remove('active');
+        profileTab.classList.add('active');
+        profileTab.classList.remove('active');
+    });
 });
